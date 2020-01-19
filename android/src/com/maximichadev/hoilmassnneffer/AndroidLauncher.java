23846fff223168863736af57f02f1b4f88b7a403
MaxiMichaDev <maxi-micha.dev@gmx.de>
2020-01-20 00:43:45 +0100

    Default libGDX project-setup.

---
+++ android/src/com/maximichadev/hoilmassnneffer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.maximichadev.hoilmassnneffer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.maximichadev.hoilmassnneffer.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ desktop/src/com/maximichadev/hoilmassnneffer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.maximichadev.hoilmassnneffer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.maximichadev.hoilmassnneffer.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
